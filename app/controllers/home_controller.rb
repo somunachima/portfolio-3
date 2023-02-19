b42e0f78a03b2e78f6e5402ecf121f1fe4eb62e3
Som Akudu <somakudu@gmail.com>
2023-02-19 20:40:59 +0000

    Transfer info from portfolio app

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,11 @@
+class HomeController < ApplicationController
+
+  def home
+  end
+
+  def resume
+  end
+
+  def about
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "home#home"
+
+  get "resume", to: "home#resume", as: "resume"
+  get "about", to: "home#about", as: "about"
 end
